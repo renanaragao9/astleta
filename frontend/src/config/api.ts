8d3bf38b12cf151b7b33c6ccb9b2f7bcd7f0e439
renanaragao9 <renanaragao159@gmail.com>
2025-12-29 20:31:23 -0300

    [FTR] - Ajustando caminho da url do backend no front

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || `${window.location.protocol}//${window.location.hostname}:7000/api`,
+    baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api',
     headers: {
         Accept: 'application/json'
     }
